1b77bb26e8030a0f74df3f4ca5219e6212be7e90
Michal Chmielewski <m.chmielewski@tut.by>
2020-02-05 01:08:54 +0100

    english version added

---
+++ website/src/components/aboutMeEn.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class AboutMeEn extends Component {
+    render() {
+        return (
+            <section className='about'>
+                <div className="photo"/>
+                <div className="description">
+                    <p>English Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deserunt exercitationem quasi quidem, quos sunt totam. Alias architecto officia sit!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae deleniti dolores earum natus nemo nisi nobis officia, temporibus ut.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa enim et eum illum odit perferendis quaerat sapiente, ullam vero.</p>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default AboutMeEn
---
+++ website/src/components/portfolioEn.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class PortfolioEn extends Component {
+    render() {
+        return (
+            <>
+                <div>English</div>
+            </>
+        )
+    }
+}
+
+export default PortfolioEn
