7314032a6d55848d19c7ef8f272614dfbcf55c15
Michal Chmielewski <m.chmielewski@tut.by>
2020-02-05 17:57:51 +0100

    scrollbar added in portfolio component

--- website/src/components/portfolio.js
+++ website/src/components/portfolio.js
@@ -3,8 +3,9 @@ import React, {Component} from "react";
 class Portfolio extends Component {
     render() {
         return (
-            <>
-            </>
+            <section className='portfolio'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A asperiores assumenda at, culpa cumque cupiditate delectus deleniti dolor doloribus ea earum esse et excepturi exercitationem facere fugit harum hic illo inventore maiores minima modi molestias nam nostrum officia omnis quia quidem quod quos reprehenderit repudiandae rerum similique tempora tenetur totam unde ut. Alias consectetur cumque debitis doloribus ducimus earum hic laudantium mollitia nam, necessitatibus nisi odit, quos sit. Accusamus aliquam animi autem culpa delectus dolores earum, eligendi est iure magnam maiores nam, nisi odio pariatur reprehenderit totam voluptates voluptatibus! Error harum magni, numquam placeat quaerat repellendus ut veniam. Perferendis, tenetur!</p>
+            </section>
         )
     }
 }
