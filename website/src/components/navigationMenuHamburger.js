77df7e9d75b294c590db71b1661e088ed37f5089
Michal Chmielewski <m.chmielewski@tut.by>
2020-04-01 20:02:51 +0200

    transition added to portfolio shades and hamburger menu

--- website/src/components/navigationMenuHamburger.js
+++ website/src/components/navigationMenuHamburger.js
@@ -6,7 +6,7 @@ class NavigationMenuHamburger extends Component {
     render() {
         if (this.props.isOpen === true) {
             return (
-                <div className="main-navigation-hamburger">
+                <div className="main-navigation-hamburger visible">
                     <div className="menu-hamburger-image">
                         <div className="image"/>
                     </div>
@@ -26,7 +26,26 @@ class NavigationMenuHamburger extends Component {
                 </div>
             )
         } else {
-            return null
+            return (
+                <div className="main-navigation-hamburger">
+                    <div className="menu-hamburger-image">
+                        <div className="image"/>
+                    </div>
+                    <ul className='menu-hamburger-container'>
+                        <li onClick={this.props.closeMenu}><NavHashLink className='nav-link' exact to='/#portfolio' activeClassName='active'
+                                                                        onClick={this.props.all}>Portfolio</NavHashLink></li>
+                        {this.props.language === "pl" ?
+                            <li onClick={this.props.closeMenu}><NavLink className='nav-link' exact to='/paulina' activeClassName='active'>O mnie</NavLink>
+                            </li> : <li onClick={this.props.closeMenu}><NavLink className='nav-link' exact to='/paulina' activeClassName='active'>About
+                                Me</NavLink></li>}
+                        {this.props.language === "pl" ?
+                            <li onClick={this.props.closeMenu}><NavLink className='nav-link' exact to='/contact' activeClassName='active'>Kontakt</NavLink>
+                            </li> :
+                            <li onClick={this.props.closeMenu}><NavLink className='nav-link' exact to='/contact' activeClassName='active'>Contact</NavLink>
+                            </li>}
+                    </ul>
+                </div>
+            )
         }
     }
 }
